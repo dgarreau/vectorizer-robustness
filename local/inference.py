influence_number runs but wrong results

local/inference.py
@@ -6,11 +6,9 @@ Functions for the inference.
 
 """
 
-import numpy as np
+import torch
 
-from scipy.special import softmax
-
-from numpy.matlib import repmat
+from torch.nn.functional import softmax
 
 from . import ParagraphVectorVariant
 
@@ -27,10 +25,10 @@ def global_neighborhood(T, winsize=5):
         (T-2*winsize,2*winsize) array with all neighborhood indices
 
     """
-    aux_0 = np.arange(winsize)
-    aux_1 = np.ones((T - 2 * winsize, 2 * winsize), dtype=int)
-    start = np.concatenate((aux_0, winsize + 1 + aux_0))
-    incr = np.cumsum(aux_1, axis=0) - aux_1
+    aux_0 = torch.arange(winsize)
+    aux_1 = torch.ones((T - 2 * winsize, 2 * winsize), dtype=torch.int)
+    start = torch.cat((aux_0, winsize + 1 + aux_0))
+    incr = torch.cumsum(aux_1, dim=0) - aux_1
     return start + incr
 
 
@@ -61,10 +59,10 @@ def global_context_vectors(example, P_matrix, model, winsize=5):
     if model == ParagraphVectorVariant.PVDMconcat:
         T = len(example)
         D = int(P_matrix.shape[1] / (2 * winsize))
-        indices = gc + D * repmat(np.arange(2 * winsize), T - 2 * winsize, 1)
-        return np.sum(P_matrix[:, indices], axis=2)
+        indices = gc + D * torch.tile(torch.arange(2 * winsize), (T - 2 * winsize, 1))
+        return torch.sum(P_matrix[:, indices], dim=2)
     elif model == ParagraphVectorVariant.PVDMmean:
-        return np.sum(P_matrix[:, gc], axis=2)
+        return torch.sum(P_matrix[:, gc], dim=2)
     else:
         print("Not implemented!")
         return 0
@@ -79,15 +77,15 @@ def global_softmax(q_vec, example, R_matrix, model=ParagraphVectorVariant.PVDBOW
     """
     T = len(example)
     if model == ParagraphVectorVariant.PVDBOW:
-        aux_h = repmat(np.dot(R_matrix, q_vec), T - 2 * winsize, 1).T
-        sm = softmax(aux_h, axis=0)
+        aux_h = torch.tile(torch.dot(R_matrix, q_vec), (T - 2 * winsize, 1)).T
+        sm = softmax(aux_h, dim=0)
     elif model == ParagraphVectorVariant.PVDMmean or model == ParagraphVectorVariant.PVDMconcat:
         gcv = global_context_vectors(example, P_matrix, model=model, winsize=winsize)
         aux_h = (
-            np.dot(R_matrix, gcv)
-            + repmat(np.dot(R_matrix, q_vec), T - 2 * winsize, 1).T
+            torch.matmul(R_matrix, gcv)
+            + torch.tile(torch.matmul(R_matrix, q_vec), (T - 2 * winsize, 1)).T
         )
-        sm = softmax(aux_h, axis=0)
+        sm = softmax(aux_h, dim=0)
     else:
         print("Not implemented!")
     return sm
@@ -101,7 +99,7 @@ def global_psi(q_vec, example, R_matrix, model=ParagraphVectorVariant.PVDBOW, P_
     aux_s = global_softmax(
         q_vec, example, R_matrix, model=model, P_matrix=P_matrix, winsize=winsize
     )
-    psi_wt = -np.log(aux_s[example[winsize : T - winsize], np.arange(T - 2 * winsize)])
+    psi_wt = -torch.log(aux_s[example[winsize : T - winsize], torch.arange(T - 2 * winsize)])
     return psi_wt
 
 
@@ -114,7 +112,7 @@ def objective_helper(
     psi_wt = global_psi(
         q_vec, example, R_matrix, model=model, P_matrix=P_matrix, winsize=winsize
     )
-    return np.sum(psi_wt)
+    return torch.sum(psi_wt)
 
 
 def compute_objective(
@@ -170,8 +168,8 @@ def global_indic(example, D, winsize=5):
         (D,T-2*winsize) array
     """
     T = len(example)
-    aux_1 = np.zeros((D, T - 2 * winsize))
-    aux_1[example[winsize : T - winsize], np.arange(T - 2 * winsize)] = 1.0
+    aux_1 = torch.zeros((D, T - 2 * winsize))
+    aux_1[example[winsize : T - winsize], torch.arange(T - 2 * winsize)] = 1.0
     return aux_1
 
 
@@ -184,7 +182,7 @@ def global_gradient(q_vec, example, R_matrix, model=ParagraphVectorVariant.PVDBO
         q_vec, example, R_matrix, model=model, P_matrix=P_matrix, winsize=winsize
     )
     aux_1 = global_indic(example, D, winsize=winsize)
-    return np.dot(R_matrix.T, aux_s - aux_1)
+    return torch.matmul(R_matrix.T, aux_s - aux_1)
 
 
 def gradient_helper(q_vec, example, R_matrix, model=ParagraphVectorVariant.PVDBOW, P_matrix=None, winsize=5):
@@ -194,7 +192,7 @@ def gradient_helper(q_vec, example, R_matrix, model=ParagraphVectorVariant.PVDBO
     all_grad = global_gradient(
         q_vec, example, R_matrix, model=model, P_matrix=P_matrix, winsize=winsize
     )
-    return np.sum(all_grad, axis=1)
+    return torch.sum(all_grad, dim=1)
 
 # TODO: Transform it in Torch code
 def compute_gradient(
@@ -217,17 +215,17 @@ def compute_gradient(
 
     if model == ParagraphVectorVariant.PVDBOW:
         if mode == "true":
-            grad = (T - 2 * winsize) * np.dot(
-                R_matrix.T, softmax(np.dot(R_matrix, q_vec))
-            ) - np.sum(R_matrix[example_orig[winsize : T - winsize], :], 0)
+            grad = (T - 2 * winsize) * torch.matmul(
+                R_matrix.T, softmax(torch.matmul(R_matrix, q_vec))
+            ) - torch.sum(R_matrix[example_orig[winsize : T - winsize], :], 0)
 
         elif mode == "linear":
-            grad_orig = (T - 2 * winsize) * np.dot(
-                R_matrix.T, softmax(np.dot(R_matrix, q_vec))
-            ) - np.sum(R_matrix[example_orig[winsize : T - winsize], :], 0)
-            grad_new = (T - 2 * winsize) * np.dot(
-                R_matrix.T, softmax(np.dot(R_matrix, q_vec))
-            ) - np.sum(R_matrix[example_new[winsize : T - winsize], :], 0)
+            grad_orig = (T - 2 * winsize) * torch.matmul(
+                R_matrix.T, softmax(torch.matmul(R_matrix, q_vec))
+            ) - torch.sum(R_matrix[example_orig[winsize : T - winsize], :], 0)
+            grad_new = (T - 2 * winsize) * torch.matmul(
+                R_matrix.T, softmax(torch.matmul(R_matrix, q_vec))
+            ) - torch.sum(R_matrix[example_new[winsize : T - winsize], :], 0)
             grad = lbda * grad_new + (1 - lbda) * grad_orig
 
         else:
@@ -264,7 +262,7 @@ def compute_gradient(
             print("Not implemented!")
     else:
         print("Not implemented!")
-        grad = np.zeros((dim,))
+        grad = torch.zeros((dim,))
     return grad
 
 
@@ -297,10 +295,10 @@ def compute_embedding(
         else:
             n_steps = 200
     D, dim = R_matrix.shape
-    q_vec = np.zeros((dim,))
-    traj_store = np.zeros((n_steps, dim))
+    q_vec = torch.zeros((dim,))
+    traj_store = torch.zeros((n_steps, dim))
     traj_store[0] = q_vec
-    obj_store = np.zeros((n_steps,))
+    obj_store = torch.zeros((n_steps,))
     if track_objective:
         obj_store[0] = compute_objective(
             q_vec,

local/models.py
@@ -214,19 +214,19 @@ class ParagraphVector(nn.Module):
         """
         Get P matrix as numpy array.
         """
-        return self._P_matrix.cpu().detach().numpy().T
+        return self._P_matrix.detach().T#.cpu().detach().numpy().T
 
     def get_Q_matrix(self):
         """
         Get Q matrix as numpy array.
         """
-        return self._Q_matrix.cpu().detach().numpy().T
+        return self._Q_matrix.detach().T#.cpu().detach().numpy().T
 
     def get_R_matrix(self):
         """
         Get R matrix as numpy array.
         """
-        return self._R_matrix.cpu().detach().numpy().T
+        return self._R_matrix.detach().T#.cpu().detach().numpy().T
 
     def load(self, name, verbose=False):
         """

plot_influence_number_replacements.py
@@ -31,7 +31,7 @@ examples = [3, 7, 10]
 
 # parameters of the experiment
 data = "IMDB"
-implem = "gensim"
+implem = "local"
 model = "PVDBOW"
 
 # get unique identifier and create relevant folders
@@ -52,7 +52,7 @@ for ex in examples:
     with open(file_name, "rb") as f:
         res_dict = pickle.load(f)
 
-    q_orig = res_dict["q_orig"]
+    q_orig = np.array(res_dict["q_orig"])
     T = len(res_dict["example_orig"])
     n_simu = res_dict["q_new_store"].shape[1]
 
@@ -60,7 +60,7 @@ for ex in examples:
     dist_store = np.zeros((T, n_simu))
     for i in range(T):
         for i_simu in range(n_simu):
-            q_new = res_dict["q_new_store"][i, i_simu]
+            q_new = np.array(res_dict["q_new_store"][i, i_simu])
             dist_store[i, i_simu] = np.linalg.norm(q_new - q_orig)
 
     mean_dist = np.mean(dist_store, axis=1)